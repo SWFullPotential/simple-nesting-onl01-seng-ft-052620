3392bf0f661f81b07a63a4d889f8be6e598ed636
Samantha Wabi <swfullpotential@gmail.com>
2020-06-01 19:19:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def hopper
 	programmer_hash = 
@@ -20,7 +20,7 @@ def hopper
     
 
 end    
-binding.pry
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
